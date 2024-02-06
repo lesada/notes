3fb616ea05bf7b593c28e1c7c3d0074ac2fe6891
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-05 23:45:52 -0300

    feat: hover and focus

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,11 +1,11 @@
 function NewNoteCard() {
   return (
-    <div className="rounded-md bg-slate-700 p-4 space-y-3">
+    <button className="rounded-md bg-slate-700 p-4 space-y-3 flex flex-col text-start outline-none relative hover:ring-2 hover:ring-slate-600  focus-visible:ring-2 focus-visible:ring-lime-400">
       <span className="text-sm font-medium text-slate-200">Add note</span>
       <p className="text-sm leading-6 text-slate-400">
         Record a audio note that will be transcribed to text automatically
       </p>
-    </div>
+    </button>
   );
 }
 
