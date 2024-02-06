8de047a38d2ac62b417c6dd2d4963b1380ab6b4b
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-05 23:33:19 -0300

    feat: notes components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Logo from "@/assets/logo-nlw-expert.svg?react";
+import NewNoteCard from "@/components/NewNoteCard";
+import NoteCard from "@/components/NoteCard";
+
 function App() {
-  return <h1 className="text-3xl font-bold">big bang</h1>;
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-8">
+      <Logo />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Search inside your notes..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+      <hr className="border-0 h-px bg-slate-700" />
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
