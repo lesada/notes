1ada06b2cedcc59c03777666ca3f4cf7c38aadf4
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-06 11:20:37 -0300

    feat: new note modal

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,11 +1,38 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import DialogContent from "./DialogContent";
+
 function NewNoteCard() {
   return (
-    <button className="rounded-md bg-slate-700 p-4 space-y-3 flex flex-col text-start outline-none relative hover:ring-2 hover:ring-slate-600  focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-200">Add note</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Record a audio note that will be transcribed to text automatically
-      </p>
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md bg-slate-700 p-4 space-y-3 flex flex-col text-start outline-none relative hover:ring-2 hover:ring-slate-600  focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-200">Add note</span>
+        <p className="text-sm leading-6 text-slate-400">
+          Record a audio note that will be transcribed to text automatically.
+        </p>
+      </Dialog.Trigger>
+      <DialogContent>
+        <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className="text-sm font-medium text-slate-200">Add note</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Start{" "}
+            <button className="font-medium text-lime-400 hover:underline">
+              recording a new note
+            </button>{" "}
+            or{" "}
+            <button className="font-medium text-lime-400 hover:underline">
+              type it manually
+            </button>
+            .
+          </p>
+        </div>
+        <button
+          type="button"
+          className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+        >
+          Salvar nota
+        </button>
+      </DialogContent>
+    </Dialog.Root>
   );
 }
 
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
-import { X } from "lucide-react";
+import DialogContent from "./DialogContent";
 
 interface NoteCardProps {
   note: {
@@ -23,30 +23,24 @@ function NoteCard({ note }: NoteCardProps) {
         <p className="text-sm leading-6 text-slate-400">{content}</p>
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/50 " />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 max-w-[640px] h-[60vw] w-full rounded-md flex flex-col outline-none overflow-hidden">
-          <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
-            <X className="size-5" />
-          </Dialog.Close>
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-200">
-              {formattedDate}
-            </span>
-            <p className="text-sm leading-6 text-slate-400">{content}</p>
-          </div>
-          <button
-            type="button"
-            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none group"
-          >
-            Do you want to{" "}
-            <span className="text-red-400 group-hover:underline">
-              delete this note
-            </span>
-            ?
-          </button>
-        </Dialog.Content>
-      </Dialog.Portal>
+      <DialogContent>
+        <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className="text-sm font-medium text-slate-200">
+            {formattedDate}
+          </span>
+          <p className="text-sm leading-6 text-slate-400">{content}</p>
+        </div>
+        <button
+          type="button"
+          className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none group"
+        >
+          Do you want to
+          <span className="text-red-400 group-hover:underline">
+            delete this note
+          </span>
+          ?
+        </button>
+      </DialogContent>
     </Dialog.Root>
   );
 }
