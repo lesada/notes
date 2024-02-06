be635d618bce1a3d91a255df11c267472310dcd1
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-06 11:42:48 -0300

    feat: start editing

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,7 +1,24 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
 import DialogContent from "./DialogContent";
 
 function NewNoteCard() {
+  const [showOnboarding, setShowOnboarding] = useState(true);
+
+  function handleStartEditor() {
+    setShowOnboarding(false);
+  }
+
+  function handleContentChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === "") setShowOnboarding(true);
+  }
+
+  function handleSaveNote(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log(event.currentTarget.content.value);
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md bg-slate-700 p-4 space-y-3 flex flex-col text-start outline-none relative hover:ring-2 hover:ring-slate-600  focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -11,26 +28,40 @@ function NewNoteCard() {
         </p>
       </Dialog.Trigger>
       <DialogContent>
-        <div className="flex flex-1 flex-col gap-3 p-5">
-          <span className="text-sm font-medium text-slate-200">Add note</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Start{" "}
-            <button className="font-medium text-lime-400 hover:underline">
-              recording a new note
-            </button>{" "}
-            or{" "}
-            <button className="font-medium text-lime-400 hover:underline">
-              type it manually
-            </button>
-            .
-          </p>
-        </div>
-        <button
-          type="button"
-          className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-        >
-          Salvar nota
-        </button>
+        <form className="flex-1 flex flex-col" onSubmit={handleSaveNote}>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-200">Add note</span>
+            {showOnboarding ? (
+              <p className="text-sm leading-6 text-slate-400">
+                Start{" "}
+                <button className="font-medium text-lime-400 hover:underline">
+                  recording a new note
+                </button>{" "}
+                or{" "}
+                <button
+                  className="font-medium text-lime-400 hover:underline"
+                  onClick={handleStartEditor}
+                >
+                  type it manually
+                </button>
+                .
+              </p>
+            ) : (
+              <textarea
+                autoFocus
+                className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChange}
+                name="content"
+              />
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          >
+            Salvar nota
+          </button>
+        </form>
       </DialogContent>
     </Dialog.Root>
   );
