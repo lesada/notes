b9fb810904e13bf0383a3925d77b29c98a4b9e95
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-07 22:57:18 -0300

    feat: responsive mobile

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
         />
       </form>
       <hr className="border-0 h-px bg-slate-700" />
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {notes.map((note) => (
           <NoteCard key={note.id} note={note} />
